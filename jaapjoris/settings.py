2a75d20c00467fda9fbdbdf6bc4e28cbcc291902
Jaap Joris Vens <jj@rtts.eu>
2020-09-12 15:19:15 +0200

    Use the latest cms cache middleware

--- jaapjoris/settings.py
+++ jaapjoris/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
+    'cms.middleware.FetchFromCacheMiddleware',
     'tidy.middleware.TidyMiddleware',
 ]
 
