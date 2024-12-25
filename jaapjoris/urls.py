e71872c3636ef4b0b7f3ab8a19488fc6786ec257
Jaap Joris Vens <jj@rtts.eu>
2024-12-25 11:08:32 +0100

    Clean up urls.py

--- jaapjoris/urls.py
+++ jaapjoris/urls.py
@@ -1,17 +1,19 @@
-from django.conf import settings
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.views.generic import RedirectView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+"""
+URLs.
+"""
 
-admin.site.site_header = settings.PROJECT_NAME.capitalize()
-admin.site.site_title = settings.PROJECT_NAME.capitalize()
-urlpatterns = staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('login/', RedirectView.as_view(url='/accounts/login/')),
-    path('logout/', RedirectView.as_view(url='/accounts/logout/')),
-    path('', include('cms.urls', namespace='cms')),
-]
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
+
+urlpatterns = (
+    staticfiles_urlpatterns()
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + [
+        path("admin/", admin.site.urls),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("", include("cms.urls", namespace="cms")),
+    ]
+)
