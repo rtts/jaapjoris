de623a1597c3b8a429a5486806b3bf7e29bc0495
Jaap Joris Vens <jj@rtts.eu>
2024-12-25 11:08:13 +0100

    Clean up models.py

--- jaapjoris/models.py
+++ jaapjoris/models.py
@@ -1,18 +1,17 @@
-from django.db import models
-from cms.models import BasePage, BaseSection
+"""
+Page and section models.
+"""
+
 from cms.decorators import page_model, section_model
+from cms.models import BasePage, BaseSection
+from django.db import models
+
 
 @page_model
 class Page(BasePage):
-    '''Add custom fields here. Already existing fields: title, slug,
-    number, menu
+    pass
 
-    '''
 
 @section_model
 class Section(BaseSection):
-    '''Add custom fields here. Already existing fields: title, type,
-    number, content, image, video, href
-
-    '''
-    page = models.ForeignKey(Page, related_name='sections', on_delete=models.PROTECT)
+    page = models.ForeignKey(Page, related_name="sections", on_delete=models.PROTECT)
